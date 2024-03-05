26eb3f692f027018e5bdd1ee855a8f524cc64d76
drnnfy <afdelaserna@mcm.edu.ph>
2024-03-06 00:00:08 +0800

    fghjk

---
+++ src/main/java/com/example/demo/mainPage.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class mainPage extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(mainPage.class.getResource("dashboard.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(fxmlLoader.load(), 876, 585);
+        stage.setScene(scene);
+        stage.show();
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logIn.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+
+         */
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
