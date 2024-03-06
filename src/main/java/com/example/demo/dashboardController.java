03bb5a95199b7975d85daffccfdd2bababfcd592
drnnfy <afDelaSerna@mcm.edu.ph>
2024-03-06 23:21:45 +0800

    fghjk

--- src/main/java/com/example/demo/dashboardController.java
+++ src/main/java/com/example/demo/dashboardController.java
@@ -1,4 +1,37 @@
 package com.example.demo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class dashboardController {
+
+    @FXML
+    private Button account;
+
+    @FXML
+    private TextField customer;
+
+    @FXML
+    private Button dashboard;
+
+    @FXML
+    private Button employee;
+
+    @FXML
+    private Button pOrder;
+
+    @FXML
+    private Button sales;
+
+    @FXML
+    private Button stocks;
+
+    @FXML
+    private Button supplier;
+
+    @FXML
+    private ComboBox<?> unit;
+
 }
